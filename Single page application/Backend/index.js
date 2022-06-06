d80b14aa660f87a6ac8ea53e5849926a7832781f
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-06-06 23:07:03 +0530

    added

--- Single page application/Backend/index.js
+++ Single page application/Backend/index.js
@@ -2,8 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-app.listen(5000, ()=> {
+
+var username = "Ambuj123";
+var pass = "Ambuj123";
+let database = "Employees";
+
+
+const connect = () => {
+    mongoose.connect(`mongodb+srv://${username}:${pass}@cluster0.xn7wc.mongodb.net/${database}?retryWrites=true&w=majority`)
+}
+
+
+
+app.listen(5000, async ()=> {
     try {
+        await connect();
         console.log("Connection Established!");
         console.log("Listening to Port 5000!");
     } catch (error) {
