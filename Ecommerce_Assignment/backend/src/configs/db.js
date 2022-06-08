b233046f567851f05de2f445e5bbc484b0f65737
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-06-08 09:58:50 +0530

    backend structure

--- Ecommerce_Assignment/backend/src/configs/db.js
+++ Ecommerce_Assignment/backend/src/configs/db.js
@@ -4,7 +4,6 @@ var username = "Ambuj123";
 var pass = "Ambuj123";
 let database = "dbdesign";
 
-
 const connect = () => {
     mongoose.connect(`mongodb+srv://${username}:${pass}@cluster0.xn7wc.mongodb.net/${database}?retryWrites=true&w=majority`)
 }
